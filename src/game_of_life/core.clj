2ecb694c9aff84237e0e58b391700eec5baf026a
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-11-19 12:26:03 +0100

    rename to 'board' to improve expressiveness

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -6,10 +6,10 @@
   (for [dx [-1 0 1] dy (if (zero? dx) [-1 1] [-1 0 1])]
     (->Cell (+ (:x cell) dx) (+ (:y cell) dy))))
 
-(defn step [cells]
-  (set (for [[loc n] (frequencies (mapcat neighbors cells))
-        :when (or (= n 3) (and (= n 2) (cells loc)))]
+(defn step [board]
+  (set (for [[loc n] (frequencies (mapcat neighbors board))
+        :when (or (= n 3) (and (= n 2) (board loc)))]
     loc)))
 
-(defn play [n cells]
-  (take (+ n 1) (iterate step cells)))
+(defn play [n board]
+  (take (+ n 1) (iterate step board)))
