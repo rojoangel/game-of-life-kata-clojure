e2c2d31b12536c61761648d6b015bd1a55723961
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-11-19 12:30:32 +0100

    reformat

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -3,13 +3,14 @@
 (defrecord Cell [x y])
 
 (defn neighbors [cell]
-  (for [x-delta [-1 0 1] y-delta (if (zero? x-delta) [-1 1] [-1 0 1])]
+  (for [x-delta [-1 0 1]
+        y-delta (if (zero? x-delta) [-1 1] [-1 0 1])]
     (->Cell (+ (:x cell) x-delta) (+ (:y cell) y-delta))))
 
 (defn step [board]
   (set (for [[loc n] (frequencies (mapcat neighbors board))
-        :when (or (= n 3) (and (= n 2) (board loc)))]
-    loc)))
+             :when (or (= n 3) (and (= n 2) (board loc)))]
+         loc)))
 
 (defn play [n board]
   (take (+ n 1) (iterate step board)))
