ce976d306b28dbae9da975fd1beb94407fb52a10
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-11-19 12:50:09 +0100

    rename boards to improve expressiveness

--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -2,13 +2,24 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]))
 
+(def vertical-line #{(->Cell 1 0)                           ;[0 1 0]
+                     (->Cell 1 1)                           ;[0 1 0]
+                     (->Cell 1 2)})                         ;[0 1 0]
+
+(def horizontal-line #{(->Cell 0 1)                         ;[0 0 0]
+                       (->Cell 1 1)                         ;[1 1 1]
+                       (->Cell 2 1)})                       ;[0 0 0]
+
+(def block #{(->Cell 1 1)                                   ;[0 0 0 0]
+             (->Cell 1 2)                                   ;[0 1 1 0]
+             (->Cell 2 1)                                   ;[0 1 1 0]
+             (->Cell 2 2)})                                 ;[0 0 0 0]
+
 (deftest game-of-life-tests
   (testing "blinker oscillator"
-    (let [board #{(->Cell 1 0) (->Cell 1 1) (->Cell 1 2)}]
-      (is (= board (first (play 1 board))))
-      (is (= #{(->Cell 0 1) (->Cell 1 1) (->Cell 2 1)} (second (play 1 board))))
-      (is (= board (nth (play 2 board) 2)))))
+    (is (= vertical-line (first (play 1 vertical-line))))
+    (is (= horizontal-line (second (play 1 vertical-line))))
+    (is (= vertical-line (nth (play 2 vertical-line) 2))))
   (testing "block still life"
-    (let [block #{(->Cell 1 1) (->Cell 1 2) (->Cell 2 1) (->Cell 2 2)}]
-      (is (= block (first (play 1 block))))
-      (is (= block (second (play 1 block)))))))
+    (is (= block (first (play 1 block))))
+    (is (= block (second (play 1 block))))))
